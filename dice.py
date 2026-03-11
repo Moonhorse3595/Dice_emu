cdf49152ed268fd6ebb62020c498d73efe83f1b0
Moonhorse3595 <timcheng20121202@gmail.com>
2026-03-11 13:48:29 +0000

    dice_class_init

---
+++ dice.py
@@ -0,0 +1,8 @@
+import secrets
+
+class dice():
+    def __init__(self, freq):
+        self.freq=freq # YOU MUST COLLECT AT LEAST 6*4*5*300 TIMES
+        # freq[6][4][5]
+    def roll(self):
+        pass # roll function
