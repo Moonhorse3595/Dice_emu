59ddfa3cee3755295abb9d841d23e3332f685806
Moonhorse3595 <timcheng20121202@gmail.com>
2026-03-11 13:52:02 +0000

    roll_template

--- dice.py
+++ dice.py
@@ -1,8 +1,11 @@
 import secrets
+from time import sleep
 
 class dice():
     def __init__(self, freq):
         self.freq=freq # YOU MUST COLLECT AT LEAST 6*4*5*300 TIMES
         # freq[6][4][5]
-    def roll(self):
-        pass # roll function
+    def roll(self, side, strength) -> int:
+        # type(side)==int
+        # type(strength)==float
+        pass
