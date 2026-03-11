8cf12fc1b8227255bb6eeed1b0ee4f96d16a5b24
Moonhorse3595 <timcheng20121202@gmail.com>
2026-03-11 14:10:26 +0000

    dice_rolling_logic2

--- dice.py
+++ dice.py
@@ -4,12 +4,17 @@ from time import sleep, time
 class dice():
     def __init__(self, freq):
         self.freq=freq # YOU MUST COLLECT AT LEAST 6*4*5*300 TIMES
-        # freq[6上面][4前面][5可能的面骰到的比例]
-    def roll(self, side, strength) -> int:
-        # type(side)==int
+        # freq[6上面][4前面][5可能的面][2标示数字和比例]
+    def roll(self, upside, frontside, strength) -> int:
+        # type(upside, frontside)==int
         # type(strength)==float
         throwing_time=(randbelow(100)+1)/100*strength
         time1=time()
         time2=time()
         while time2-time1<=throwing_time:
-            pass
+            lst=[]
+            for i in self.freq[upside-1][frontside-1]:
+                for j in i[1]:
+                    lst.append(str(i[0])) # list was been finished
+            # roll
+            # change upside&frontside
